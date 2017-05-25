c81f194f03e552d125a937f13366fd5f356c74d2
cwis <cwiiss+github@gmail.com>
2017-05-25 18:42:24 +0200

    Improve compatibility with NodeBB v1.5

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 "use strict";
 
 var Categories = module.parent.require('./categories');
+var db = module.parent.require('./database');
 
 exports.filterTopicsPrepare = function(data, callback) {
 	var sets = typeof data.set === "string"? [data.set]: data.set;
@@ -12,10 +13,33 @@ exports.filterTopicsPrepare = function(data, callback) {
 				sets.push("cid:" + child.cid + ":tids");
 			});
 		}
-		data.set = sets;
+		data.set = [ { unionArray: sets } ];
 		return callback(null, data);
 	});
 };
 
+exports.init = function init(callback) {
+	var orig_getSortedSetRevIntersect = db.getSortedSetRevIntersect;
+	db.getSortedSetRevIntersect = function(params, callback) {
+		var sets = params.sets;
+		if(Array.isArray(sets) && sets[0] && sets[0].unionArray) {
+			return db.getSortedSetRevRange(sets[0].unionArray, params.start, params.stop, callback);
+		} else {
+			return orig_getSortedSetRevIntersect.apply(this, arguments);
+		}
+	};
+
+	var orig_getSortedSetIntersect = db.getSortedSetIntersect;
+	db.getSortedSetIntersect = function(params, callback) {
+		var sets = params.sets;
+		if(Array.isArray(sets) && sets[0] && sets[0].unionArray) {
+			return db.getSortedSetRange(sets[0].unionArray, params.start, params.stop, callback);
+		} else {
+			return orig_getSortedSetIntersect.apply(this, arguments);
+		}
+	};
+
+	return callback();
+};
 
 })();
